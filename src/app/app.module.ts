55567169716f054d8bb73f3052b74e6288cc8c9b
Daire Finn <daire.finn@mail.itsligo.ie>
2018-11-08 14:33:47 +0000

    New project w/ sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from 'src/app/components/navbar/navbar.component'
-import { SidebarComponent } from 'src/app/components/sidebar/sidebar.component'
-import { AppRouterModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    SidebarComponent
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRouterModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
